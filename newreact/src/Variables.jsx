c9ece153c674b487f6120ce112f8bd6bf0cf819b
sivaramaiahpupalla <sivaramaiahpuppalla@gmail.com>
2024-12-19 10:09:55 +0530

    After changes to calculator operations

--- newreact/src/Variables.jsx
+++ newreact/src/Variables.jsx
@@ -7,18 +7,59 @@ const [input,setInput] = useState({
   b:"",
 });
  
-const {a,b} = input;
+ const {a,b} = input;
 
-   const [submitval,setSubmitval] = useState(false);
+ const [submitval,setSubmitval] = useState(false);
+ const [inputone,setInputone] = useState(false);
+ const [inputtwo,setInputTwo] = useState(false);
+ const [inputthree,setInputThree] = useState(false);
+ const [inputfour,setInputFour] = useState(false);
 
-  const handleChangeOne = (event) => {
-    setInput({...input, [event.target.name]:[event.target.value]});
-      }
+ const handleChangeOne = (event) => {
+   setInput({...input, [event.target.name]:[event.target.value]});
+ }
       
 
   const handlePrint = (event) => {
-       setSubmitval(true) 
-  }
+       setSubmitval(true); 
+   }
+   const handleAdd = (event) => {
+    const num1 = parseFloat(a);
+    const num2 = parseFloat(b);
+    const r1 = num1+num2;
+      setInputone(r1) 
+    }
+    
+  
+
+   const handleSub = (event) => {
+    const num1 = parseFloat(a);
+    const num2 = parseFloat(b);
+    const r2 = num1-num2;
+      if(num2>num1)
+      {
+        setInputTwo(r2);
+      }
+      else {
+        console.log("Wrong inputs");
+        
+      }
+       
+   }
+
+    const handleMul = (event) => {
+      const num1 = parseFloat(a);
+      const num2 = parseFloat(b);
+      const r3 = num1*num2;
+        setInputThree(r3)  
+    }
+    const handleDiv = (event) => {
+      const num1 = parseFloat(a);
+      const num2 = parseFloat(b);
+      const r4 = num1/num2;
+        setInputFour(r4) 
+     
+    }
   
   return (
     <>
@@ -33,8 +74,16 @@ const {a,b} = input;
     </div>
     <div>
       <button  onClick={handlePrint}>print</button>
+      <button  onClick={handleAdd}>ADD</button>
+      <button  onClick={handleSub}>SUB</button>
+      <button  onClick={handleMul}>MUL</button>
+      <button  onClick={handleDiv}>DIV</button>
     </div>
     {submitval && (<p>A value is :{input.a} and B  value is :{input.b}</p>)}
+    {inputone && (<p>result:{input.a}+{input.b}={handleAdd}</p>)}
+    {inputtwo && (<p>result:{input.a}-{input.b}</p>)}
+    {inputthree && (<p>result:{input.a}*{input.b}</p>)}
+    {inputthree && (<p>result:{input.a}/{input.b}</p>)}
     </>
   )
 }
