4d9d506198410be881d9359314d189810f002fd8
sivaramaiahpupalla <sivaramaiahpuppalla@gmail.com>
2025-01-08 09:49:24 +0530

    After adding the popup to crud operations

--- newreact/src/App.js
+++ newreact/src/App.js
@@ -1,16 +1,34 @@
 import React from 'react'
-import Profile from './Profile';
+
+// import Profile from './Profile';
 import Crud from './Crud';
+// import Home from './Home';
+// import About from './About';
+// import Contact from './Contact';
 
 const App = () => {
   return (
     <>
     {/* <Variables /> */}
     {/* <Profile /> */}
-    <Crud />
+     <Crud /> 
+    {/* <Home />
+    <About />
+    <Contact />  */}
     </>
   )
 }
 
 export default App;
 
+// import PopupExample from "./PopupExample";
+
+// const App = () => {
+//   return (
+//     <>
+//       <PopupExample />
+//     </>
+//   );
+// };
+
+// export default App;
--- newreact/src/Crud.jsx
+++ newreact/src/Crud.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-
+import './Crud.css';
 const Crud = () => {
   const [stu, setStu] = useState({
     FirstName: '',
@@ -13,6 +13,7 @@ const Crud = () => {
   const { FirstName, LastName, DOB, Qualification, PassedoutYear, Percentage } = stu;
   const [data, setData] = useState([]);
   const [editId, setEditId] = useState(null);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
 
   const fetchData = () => {
     axios.get('http://localhost:5000/users')
@@ -67,6 +68,7 @@ const Crud = () => {
         Percentage: '',
       });
       setEditId(null);
+      setIsPopupOpen(false);
     } catch (error) {
       console.log(error);
     }
@@ -84,38 +86,57 @@ const Crud = () => {
   
   const handleEdit = async (id) => {
     const rowToEdit = data.find((item) => item.id === id);
-    setStu(rowToEdit);
+   // setStu(rowToEdit);
     setEditId(id);
+    setIsPopupOpen(true);
   };
+
+  const handleClosePopup = () => {
+    setIsPopupOpen(false);
+    setStu({
+        
+        FirstName: '',
+        LastName: '',
+        DOB: '',
+        Qualification: '',
+        PassedoutYear: '',
+        Percentage: '',
+      });
+       setEditId(null);
+  };
+  const handlePopupOpen = () => {
+     setIsPopupOpen(true);
+  };
+
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <div>
+      <form className='form' onSubmit={handleSubmit}>
+        <div className='FirstNameField'>
           <label htmlFor="FirstName">FirstName :</label>
           <input type="text" value={FirstName} name="FirstName" onChange={handleChange} />
         </div>
-        <div>
+        <div className='LastNameField'>
           <label htmlFor="LastName">LastName :</label>
           <input type="text" value={LastName} name="LastName" onChange={handleChange} />
         </div>
-        <div>
+        <div className='DOBField'>
           <label htmlFor="DOB">DOB :</label>
           <input type="text" value={DOB} name="DOB" onChange={handleChange} />
         </div>
-        <div>
+        <div className='QualificationField'>
           <label htmlFor="Qualification">Qualification :</label>
           <input type="text" value={Qualification} name="Qualification" onChange={handleChange} />
         </div>
-        <div>
+        <div className='PassedoutYearField'>
           <label htmlFor="PassedoutYear">PassedoutYear :</label>
           <input type="text" value={PassedoutYear} name="PassedoutYear" onChange={handleChange} />
         </div>
-        <div>
+        <div className='PercentageField'>
           <label htmlFor="Percentage">Percentage :</label>
           <input type="text" value={Percentage} name="Percentage" onChange={handleChange} />
         </div>
-        <div>
+        <div className='SubmitButton'>
           <button type="submit">{editId ? "Update" : "Submit"}</button>
         </div>
       </form>
@@ -141,8 +162,8 @@ const Crud = () => {
               <td style={{ border: '1px solid black', padding: '8px' }}>{item.Qualification}</td>
               <td style={{ border: '1px solid black', padding: '8px' }}>{item.PassedoutYear}</td>
               <td style={{ border: '1px solid black', padding: '8px' }}>{item.Percentage}</td>
-              <td>
-                <button onClick={() => handleEdit(item.id)}>edit</button>
+              <td style={{ border: '1px solid black', padding: '8px' }}>
+                <button onClick={() => handleEdit(item.id)} >edit</button>
                 <span> </span>
                 <button onClick={() => handleDelete(item.id)}>delete</button>
               </td>
@@ -150,6 +171,77 @@ const Crud = () => {
           ))}
         </tbody>
       </table>
+      {isPopupOpen && (
+        <div className='popupclosestyle'
+          
+          onClick={handleClosePopup}
+        >
+          <div className='popupcontentstyle'
+           
+            onClick={(e) => e.stopPropagation()}
+          >
+            <h2>Edit Student Details</h2>
+            <form onSubmit={handleUpdate}>
+              <div>
+                <label>FirstName:</label>
+                <input
+                  type="text"
+                  name="FirstName"
+                  value={FirstName}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <label>LastName:</label>
+                <input
+                  type="text"
+                  name="LastName"
+                  value={LastName}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <label>DOB:</label>
+                <input
+                  type="text"
+                  name="DOB"
+                  value={DOB}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <label>Qualification:</label>
+                <input
+                  type="text"
+                  name="Qualification"
+                  value={Qualification}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <label>PassedoutYear:</label>
+                <input
+                  type="text"
+                  name="PassedoutYear"
+                  value={PassedoutYear}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <label>Percentage:</label>
+                <input
+                  type="text"
+                  name="Percentage"
+                  value={Percentage}
+                  onChange={handleChange}
+                />
+              </div>
+              <button type="submit">Update</button>
+            </form>
+            <button onClick={handleClosePopup}>Cancel</button>
+          </div>
+        </div>
+      )}
     </>
   );
 };
